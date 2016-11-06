9b59cd467c3513759b6e6df5c17eec415dd3f80e
LVA <D:\ARHIV - program\ \The BAT!\The Bat\Mail>
2016-11-07 00:02:19 +0200

    Initial commit

---
+++ src/main/java/VITALII_LEVKOVYCH/DBConnection.java
@@ -0,0 +1,27 @@
+package VITALII_LEVKOVYCH;
+
+/**
+ * Created by Виталий on 06.11.2016.
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
+public class DBConnection {
+    private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_CONNECTION = "jdbc:mysql://127.0.0.1:3306/BD_Books";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "root";
+
+
+
+}
